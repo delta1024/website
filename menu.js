da1ac121f971bfc7ae8b3e649a79d07f4eff5361
Jacob Stannix <jakestannix at gmail.com>
2022-02-13 13:51:35 -0700

    menu as js file

---
+++ menu.js
@@ -0,0 +1,11 @@
+document.write(`  
+  <div class="sidebar">
+    <u>    
+      <heading>Navigation</heading>
+    </u>
+    <br>
+    <nav>
+      <a href="https://jakestannix.org">Home</a>
+    </nav>
+  </div>
+`);
